0319dccee8e435437621c99df7a14b88b7227f50
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-03 13:02:19 -0800

    Add some more complex tests.

--- db/sqlite/test_client.py
+++ db/sqlite/test_client.py
@@ -34,9 +34,49 @@ class ClientTestCase(unittest.TestCase):
             self.client.create_recipe(recipe)
 
     def test_get_returns_none_if_no_recipe_inserted_yet(self):
+        self.assertIsNone(self.client.get_recipe("cake"))
         recipe = data_layer.StructuredRecipe(name="Pascetti", recipe_ingredients=set())
         self.client.create_recipe(recipe)
+        self.assertIsNone(self.client.get_recipe("cake"))
 
+    def test_get_recipes_with_overlapping_ingredients(self):
+        # Bread
+        bread_recipe = data_layer.StructuredRecipe(
+            "bread",
+            recipe_ingredients={
+                data_layer.RecipeIngredient("flour", 10),
+                data_layer.RecipeIngredient("water", 7),
+                data_layer.RecipeIngredient("salt", 1),
+            },
+        )
+        # Cake
+        cake_recipe = data_layer.StructuredRecipe(
+            "cake",
+            recipe_ingredients={
+                data_layer.RecipeIngredient("flour", 5),
+                data_layer.RecipeIngredient("water", 7),
+                data_layer.RecipeIngredient("egg", 2),
+                data_layer.RecipeIngredient("sugar", 4),
+                data_layer.RecipeIngredient("salt", 1),
+            },
+        )
+        # Spaghetti
+        spaghetti_recipe = data_layer.StructuredRecipe(
+            "spaghetti",
+            recipe_ingredients={
+                data_layer.RecipeIngredient("flour", 8),
+                data_layer.RecipeIngredient("water", 2),
+                data_layer.RecipeIngredient("egg", 5),
+                data_layer.RecipeIngredient("salt", 1),
+            },
+        )
+        self.client.create_recipe(bread_recipe)
+        self.client.create_recipe(cake_recipe)
+        self.client.create_recipe(spaghetti_recipe)
+
+        self.assertEqual(bread_recipe, self.client.get_recipe("bread"))
+        self.assertEqual(cake_recipe, self.client.get_recipe("cake"))
+        self.assertEqual(spaghetti_recipe, self.client.get_recipe("spaghetti"))
 
 
 if __name__ == "__main__":
