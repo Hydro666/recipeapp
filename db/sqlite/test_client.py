3a43fdf4196c141e34d1b7b6b707adbd77573684
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-03 12:55:54 -0800

    Fix tests.

--- db/sqlite/test_client.py
+++ db/sqlite/test_client.py
@@ -17,11 +17,26 @@ class ClientTestCase(unittest.TestCase):
     def test_create_and_get_recipe_with_ingredients(self):
         recipe = data_layer.StructuredRecipe(
             name="cake",
-            recipe_ingredients={(4, "flour"), (3, "sugar"), (1, "salt")},
+            recipe_ingredients={
+                data_layer.RecipeIngredient("flour", 4),
+                data_layer.RecipeIngredient("sugar", 3),
+                data_layer.RecipeIngredient("salt", 1),
+            },
         )
         self.client.create_recipe(recipe)
-        actual = self.client.get_recipe(recipe.name)
+        actual = self.client.get_recipe("cake")
         self.assertEqual(recipe, actual)
+
+    def test_create_two_recipes_with_same_name(self):
+        recipe = data_layer.StructuredRecipe(name="Pascetti", recipe_ingredients=set())
+        self.client.create_recipe(recipe)
+        with self.assertRaises(client.SQLiteException):
+            self.client.create_recipe(recipe)
+
+    def test_get_returns_none_if_no_recipe_inserted_yet(self):
+        recipe = data_layer.StructuredRecipe(name="Pascetti", recipe_ingredients=set())
+        self.client.create_recipe(recipe)
+
 
 
 if __name__ == "__main__":
