ba05aaadcda2f60326b2e77aaf1e6b9fa4489900
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-11 20:32:28 -0800

    Add placeholder functionality to react componenet.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,46 @@
 /// <reference types="vite-plugin-svgr/client" />
 import logo from './logo.svg?react';
 import './App.css';
+import { useState } from 'react';
+
+const baseUrl = "http://localhost:5000"
+
+
+const fixedData = {
+  "name": "Bread",
+  "recipe_ingredients": [
+    {"name": "Flour", "quantity": 100},
+    {"name": "Water", "quantity": 80},
+    {"name": "Salt", "quantity": 2},
+    {"name": "yeast", "quantity": 1},
+  ]
+}
+
+function Recipebox({ recipeData }) {
+  const ingredients = recipeData.recipe_ingredients.map(ri => 
+    <li key="2">
+      {ri.name} : {ri.quantity}
+    </li>
+  );
+  return (
+    <>
+      <h1>Recipe for: {recipeData.name}</h1>
+      <ul>
+        {ingredients}
+      </ul>
+    </>
+  )
+}
 
 function App() {
+  const [recipeState, setRecipeState] = useState(fixedData);
+
+  async function oonClick(e) {
+    const response = await fetch(baseUrl + "/recipe/cake");
+    const json = await response.json();
+    setRecipeState(json);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +48,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button onClick={oonClick}>Get dat cake</button>
+        <Recipebox recipeData={recipeState}/>
         <a
           className="App-link"
           href="https://reactjs.org"
