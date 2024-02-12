784ec7a17a0dfd6e25b5408443e7d644e91ab8d9
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-11 18:48:12 -0800

    Add test case.

--- db/sqlite/test_client.py
+++ db/sqlite/test_client.py
@@ -93,6 +93,43 @@ class ClientTestCase(absltest.TestCase):
         self.assertEqual(cake_recipe, self.client.get_recipe("cake"))
         self.assertEqual(spaghetti_recipe, self.client.get_recipe("spaghetti"))
 
+    def test_list_three_inserted_recipes(self):
+        # Bread
+        bread_recipe = data_layer.StructuredRecipe(
+            "bread",
+            recipe_ingredients={
+                data_layer.RecipeIngredient("flour", 10),
+                data_layer.RecipeIngredient("water", 7),
+                data_layer.RecipeIngredient("salt", 1),
+            },
+        )
+        # Cake
+        cake_recipe = data_layer.StructuredRecipe(
+            "cake",
+            recipe_ingredients={
+                data_layer.RecipeIngredient("flour", 5),
+                data_layer.RecipeIngredient("water", 7),
+                data_layer.RecipeIngredient("egg", 2),
+                data_layer.RecipeIngredient("sugar", 4),
+                data_layer.RecipeIngredient("salt", 1),
+            },
+        )
+        # Spaghetti
+        spaghetti_recipe = data_layer.StructuredRecipe(
+            "spaghetti",
+            recipe_ingredients={
+                data_layer.RecipeIngredient("flour", 8),
+                data_layer.RecipeIngredient("water", 2),
+                data_layer.RecipeIngredient("egg", 5),
+                data_layer.RecipeIngredient("salt", 1),
+            },
+        )
+        self.client.create_recipe(bread_recipe)
+        self.client.create_recipe(cake_recipe)
+        self.client.create_recipe(spaghetti_recipe)
+
+        self.assertListEqual(["bread", "cake", "spaghetti"], self.client.list_recipes())
+
     def test_update_recipe_with_no_target(self):
         recipe = data_layer.StructuredRecipe(name="Pascetti", recipe_ingredients=set())
         with self.assertRaises(client.SQLiteException):
