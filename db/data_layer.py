3528e16f131a4eb24fcaa6c70e39291296792da4
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-11 18:08:55 -0800

    Implement.

--- db/data_layer.py
+++ db/data_layer.py
@@ -1,5 +1,5 @@
 import abc
-from typing import NamedTuple
+from typing import Any, NamedTuple
 
 
 class RecipeIngredient(NamedTuple):
@@ -10,6 +10,34 @@ class RecipeIngredient(NamedTuple):
 class StructuredRecipe(NamedTuple):
     name: str
     recipe_ingredients: set[RecipeIngredient]
+
+
+class DataSerializer:
+    """Class for serializing and deserializing a StructuredRecipe tuple as a JSON.
+    
+    The zero value for the `recipe_ingredients` will be the empty list and not
+    null, or field absence.
+    
+    The order of the elements in the `recipe_ingredients` does not hold any
+    semantic meaning."""
+
+    def serialize_structured_recipe_to_json(self, recipe: StructuredRecipe) -> dict[str, Any]:
+        return {
+            'name': recipe.name,
+            'recipe_ingredients': [
+                {"name": ri.name, "quantity": ri.quantity} for ri in recipe.recipe_ingredients
+            ]
+        }
+    
+    def parse_json_to_structured_recipe(self, json: dict[str, Any]) -> StructuredRecipe:
+        if set(json) != {'name', 'recipe_ingredients'}:
+            raise ValueError(f"Json: {json} cannot be parsed into a structured recipe!")
+        if type(json['recipe_ingredients']) is not list:
+            raise ValueError(f"Json: {json} cannot be parsed into a structured recipe!")
+        ris = {RecipeIngredient(x['name'], x['quantity']) for x in json['recipe_ingredients']}
+        if len(ris) != len(json['recipe_ingredients']):
+            raise ValueError(f"Json: {json} cannot be parsed into a structured recipe!")
+        return StructuredRecipe(json['name'], ris)
 
 
 class DataAccessClient:
