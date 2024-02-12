ac08228488bd18a23c09ed5db7c9398e980a3f6a
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-11 20:53:49 -0800

    Remove fluff

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -45,19 +45,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <button onClick={oonClick}>Get dat cake</button>
         <Recipebox recipeData={recipeState}/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
