a3594de74e4b295133fe6df714644b93c7a0f105
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-11 18:45:05 -0800

    Minimal flask server working.

--- server/flask_server.py
+++ server/flask_server.py
@@ -3,6 +3,7 @@ from db.sqlite import client
 
 from absl import app
 from absl import flags
+from absl import logging
 import flask
 
 FLAGS = flags.FLAGS
@@ -14,13 +15,13 @@ flags.mark_flag_as_required("sqlite_db_path")
 def create_flask(test_config=None):
     flask_app = flask.Flask(__name__)
 
-    db_connection = client.SQLiteClient(FLAGS.sqlite_db_path)
-
     @flask_app.route("/recipe/<name>")
     def get_recipe(name: str):
+        db_connection = client.SQLiteClient(FLAGS.sqlite_db_path)
+        logging.info("Trying to get recipe with name: %s", name)
         recipe = db_connection.get_recipe(name)
         if recipe is None:
-            flask.abort(401)
+            flask.abort(404)
         return data_layer.DataSerializer().serialize_structured_recipe_to_json(recipe)
     
     return flask_app
