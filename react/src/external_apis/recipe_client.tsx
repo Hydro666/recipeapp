3232f0517fb7720dc844d722d798c65249f9b43b
Henry Lancelle <henry.lancelle@gmail.com>
2024-02-20 23:41:26 -0800

    Add BUILD and another method for client.

--- react/src/external_apis/recipe_client.tsx
+++ react/src/external_apis/recipe_client.tsx
@@ -1,4 +1,4 @@
-export class RecipeClient {
+export default class RecipeClient {
 
     constructor(
         private readonly baseUrl: string
@@ -6,6 +6,13 @@ export class RecipeClient {
 
     public async listRecipes() {
         let uri = new URL('/recipes', this.baseUrl);
+        let response = await fetch(uri);
+        let json = await response.json();
+        return json;
+    }
+
+    public async getRecipe(name: string) {
+        let uri = new URL(`/recipe/${name}`, this.baseUrl);
         let response = await fetch(uri);
         let json = await response.json();
         return json;
